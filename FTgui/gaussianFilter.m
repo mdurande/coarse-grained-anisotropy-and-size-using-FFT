4457fabc2a700df51ce41df8df0a7ac37d3c6b8b
mdurande <melina.durande@univ-lyon1.fr>
2019-03-20 17:20:29 +0100

    Delete gaussianFilter.m

--- FTgui/gaussianFilter.m
+++
@@ -1,7 +0,0 @@
-function filter = gaussianFilter(r,s)
-% Make a gaussian filter of radius 'r' and standard deviation 's'
-%   filter = gaussianFilter(r,s)
-
-[x,y]  = meshgrid(-r:r,-r:r);
-filter = exp(-(x.^2 + y.^2)/(2*s.^2));
-filter = filter./sum(filter(:));
