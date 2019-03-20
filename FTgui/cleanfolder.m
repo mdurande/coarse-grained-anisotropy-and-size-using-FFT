3f25cc7558748f87c19e4b8ab273f6863fc1d54e
mdurande <melina.durande@univ-lyon1.fr>
2019-03-20 17:19:35 +0100

    Delete cleanfolder.m

--- FTgui/cleanfolder.m
+++
@@ -1,5 +0,0 @@
-function cleanednames = cleanfolder(folder)
-    int1 = dir(folder);
-    int1([int1.isdir]) = [];
-    A = strfind({int1.name},'.DS_Store');
-    cleanednames = int1(cellfun('isempty',A));
